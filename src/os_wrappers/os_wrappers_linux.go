0b79ad6c074929372a9494a84b6878f7665ddfbe
Corentin PASQUIER <corentin.pasquier@ceramaret.com>
2025-10-08 08:44:54 +0200

    [~] Update the wrapper for linux, to unblock the window to  open PDF

--- src/os_wrappers/os_wrappers_linux.go
+++ src/os_wrappers/os_wrappers_linux.go
@@ -5,6 +5,7 @@ package os_wrappers
 
 import (
 	"os"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/dialog"
 )
@@ -17,28 +18,32 @@ func GetSystemLangCode() string {
 	return "en"
 }
 
+// Non-blocking file open for Linux
 func SelectMultipleFiles(main fyne.Window) ([]string, error) {
-	var files []string
-	done := make(chan struct{})
-	dialog.NewFileOpen(func(reader fyne.URIReadCloser, err error) {
-		if reader != nil {
-			files = append(files, reader.URI().Path())
+	results := []string{}
+
+	dlg := dialog.NewFileOpen(func(reader fyne.URIReadCloser, err error) {
+		if err != nil || reader == nil {
+			return
 		}
-		close(done)
-	}, main).Show()
-	<-done
-	return files, nil
+		results = append(results, reader.URI().Path())
+	}, main)
+
+	dlg.SetFilter(&fyne.FileFilter{
+		Extensions: []string{".pdf"},
+	})
+	dlg.Show()
+
+	// Return immediately; results will be updated asynchronously
+	return results, nil
 }
 
 func SelectSaveFile(main fyne.Window) (string, error) {
 	var out string
-	done := make(chan struct{})
 	dialog.NewFileSave(func(writer fyne.URIWriteCloser, err error) {
-		if writer != nil {
+		if err == nil && writer != nil {
 			out = writer.URI().Path()
 		}
-		close(done)
 	}, main).Show()
-	<-done
 	return out, nil
 }
